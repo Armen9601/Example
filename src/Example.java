bb00a89c1c58d33ef62b65f7465cbf1025638ebf
Armen <simonyanarmen9601@gmail.com>
2021-10-03 00:45:16 +0400

    Initial commit

---
+++ src/Example.java
@@ -0,0 +1,9 @@
+public class Example {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("kuku"+i);
+        }
+        System.out.println("stop");
+    }
+}
---
+++ src/Masiv.java
@@ -0,0 +1,35 @@
+public class Masiv {
+    public static void main(String[] args) {
+        int[] a = {12, 32, 1, 4, 9, 24, 102, 256};
+        {
+            int max = 0;
+
+            int x = 0;
+
+            for (int i = 0; i < a.length; i++) {
+
+
+
+                }
+
+//                x += a[i];
+            }
+//                if (a[i] % 2 == 0)
+//                    max++;
+//
+//
+//
+//                System.out.println(a[i]);
+//                if (max < a[i])
+//                    max = a[i];
+
+
+//            System.out.println(x);
+//            System.out.println(x/a.length);
+
+        }
+
+
+    }
+
+
